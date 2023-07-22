e6ac0a38943557d3aff6108add2d6bc1999bf29d
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2023-07-22 22:12:09 +0100

    redo

--- main.h
+++ main.h
@@ -18,48 +18,7 @@ typedef struct format
 //formatting whatever the user gives us
 int _print(const char *format, ...);
 
-//formating one character
-int _putChar(char c);
 
-//printing a char
-int printChar(va_list val);
 
-//formating a whole string
-int printString(va_list val);
-
-//printing a char as a sring
-//void _printStr(const char *str);
-
-//get the length of a constant char
-int _strLenc(const char *s);
-
-//get the length of a string
-int _strLen(char *s);
-
-int print_37(void);
-
-int print_HEX(va_list val);
-
-int print_bin(va_list val);
-
-int print_exclusive_string(va_list val);
-
-int print_hex_aux(unsigned long int num);
-
-int print_int(va_list args);
-
-int print_dec(va_list args);
-
-int print_oct(va_list val);
-
-int print_pointer(va_list val);
-
-int print_pointer(va_list val);
-
-int print_rot13(va_list args);
-
-int print_srev(va_list args);
-
-int print_unsigned(va_list args);
 
 #endif // MAIN_H
